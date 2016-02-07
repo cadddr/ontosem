48bf816cc737880bac54bb11a9588c788d446d16
Ethan Bond <me@ethanbond.com>
2016-02-07 15:43:00 -0500

    Rename templates to views, get dust templating functional

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ var template = require('./templateengine.js');
 module.exports = {
   index: function(req, res) {
     log.info("Serving INDEX");
-    template("layout", {test: "Good test!"}, res.send);
+    res.render("layout", {test: "Good test!"});
 
   }
 };
