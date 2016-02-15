a535dddea51efcb1dae5fc699f950e11c13c3038
Ethan Bond <me@ethanbond.com>
2016-02-15 13:57:51 -0500

    Reformat and output attributes to DOM as keyvalue pairs

--- tmr.js
+++ tmr.js
@@ -11,20 +11,32 @@ module.exports = {
     // JSON object to render the decorated TMR
     // to the browser
 
-    var  o = {};
-    o.tmrs = [];
+    var  o = [];
 
     log.info("Interpreting TMR...");
 
     var frames = Object.keys(tmr);
     // Get an array of the frame heads
 
-    frames.forEach(function(frame){
-      tmr[frame]._key = frame;
-      o.tmrs.push(tmr[frame]);
+    frames.forEach(function(frameName){
+      var p = {};
+      var frame = tmr[frameName];
+      p._key = frameName;
+      p.attrs = [];
+      p.optional = [];
+
+      Object.keys(frame).forEach(function(attrKey){
+        var attrVal = frame[attrKey];
+        if(utils.isCapitalized(attrKey)){
+          p.attrs.push({key: attrKey, val: attrVal});
+        } else {
+          p.optional.push({key: attrKey, val: attrVal});
+        }
+      });
+      o.push(p);
     });
 
     log.info(o)
-    return o;
+    return {tmrs: o};
   }
 };
