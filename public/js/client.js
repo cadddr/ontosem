d1e5eb7772b56358985cc6f81ea242089199f2f4
Ethan Bond <me@ethanbond.com>
2016-03-06 18:11:28 -0500

    Get rid of stale code

--- public/js/client.js
+++ public/js/client.js
@@ -20,51 +20,6 @@ toggle.addEventListener("click", function(e){
 
 var data = JSON.parse($("#data-sync")[0].textContent);
 
-/* Not used anymore with sentence given entity name and id (I think)
-// ********************************************
-// This should probably be done on the server
-var annotateSentence = function(){
-  var sentence = $("#sentence")[0].innerText.split(" ");
-  var annotatedSentence = [];
-
-  for(index in sentence){
-    annotatedSentence.push({token: sentence[index], id: -1});
-  }
-
-  for(index in data.tmrs){
-    var tmr = data.tmrs[index];
-    var optionalAttrs = tmr.optional;
-    var wordIndex = -1;
-    optionalAttrs.forEach(function(kvpair){
-      if(kvpair.key == "word-ind"){ wordIndex = kvpair.val; }
-    });
-
-    if(wordIndex != -1){
-      annotatedSentence[wordIndex]._id = tmr._id;
-    }
-  }
-
-  return annotatedSentence;
-};
-// ********************************************
-var reconstructSentence = function(annotatedSentence){
-  var htmlString = "";
-
-  for(index in annotatedSentence){
-    var chunk = annotatedSentence[index];
-    htmlString += "<span data-entity-id='";
-    htmlString += chunk._id;
-    htmlString += "'>";
-    htmlString += chunk.token;
-    htmlString += "</span> ";
-  }
-
-  return htmlString;
-};
-
-$("#sentence")[0].innerHTML = reconstructSentence(annotateSentence());
-*/
-
 var entities = data.entities;
 
 $("[data-entity-id]").on("mouseenter", function(e){
