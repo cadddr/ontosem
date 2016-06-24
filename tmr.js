c123e68805fd8154b6e05614cd0386795583b9b2
wongc7 <wongc7@rpi.edu>
2016-06-23 21:50:53 -0400

    added sentence dissection to preserve punctuation and spacing, fixed entity sorting, removed unnecessary dust files, updated packages.json

--- tmr.js
+++ tmr.js
@@ -5,12 +5,36 @@ var log = utils.richLogging;
 var relations = new Set(Object.keys(utils.inverseMap));
 var debugKeys = new Set(["is-in-subtree","syn-roles","lex-source","concept", "word-ky"]);
 
-function sortFrames(frames) {
-	var sortedFrames = [];
-	for (var i = frames.length; i >= 0; --i) {
-		for (var child in frames[i].insertAfter)
-			;
+function dissectSentences(sentence) {
+	var sentenceRegExp = /(.*?)([!?.](?:\s|$))/g;
+	var wordRegExp = /('*\w+)(\s*)/g;
+	var outerResult = [];
+	var sentences = [];
+
+	while (outerResult = sentenceRegExp.exec(sentence)) {
+		console.log(outerResult);
+		var words = [];
+		var innerResult = [];
+		while (innerResult = wordRegExp.exec(outerResult[1]))
+			words.push({"_token": innerResult[1], "_spacing": innerResult[2]});
+		sentences.push({"words": words, "_punct": outerResult[2]});
 	}
+
+	log.attn(sentence);
+	console.log(sentences);
+	return sentences;
+}
+
+function sortFrames(frames) {
+	var modality = [];
+	var events = [];
+	for (var i = frames.length-1; i >= 0; --i) {
+		if (!(frames[i].attributes.debug.hasOwnProperty("is-in-subtree")))
+			modality.push(frames.splice(i, 1)[0]);
+		else if (frames[i].attributes.debug["is-in-subtree"] == "EVENT")
+			events.push(frames.splice(i, 1)[0]);
+	}
+	var sortedFrames = modality.concat(events, frames);
 	return sortedFrames;
 }
 
@@ -29,15 +53,11 @@ function insertLinebreaks(s) {
 
 module.exports = {
 	format: function(data) {
-//		log.info('data:');
-//		log.info(data);
 		// Passed a raw JSON TMR, returns a formatted and annotated
 		// JSON object to render the decorated TMR to the browser
 
 		var sentenceId = data.sentenceId;
-		var splitSentence = data.sentence.split(" ").map(function(token) {
-			return {"_token": token};
-		});
+		var sentences = dissectSentences(data.sentence);
 		var tmrIndex = data.tmrIndex;
 		var tmr = data.tmr;
 
@@ -45,6 +65,7 @@ module.exports = {
 
 		var frames = [];
 		var entitySet = new Set(Object.keys(tmr));
+		var sentOffset = -1;
 
 		var color = {};
 		var colorCounter = 0;
@@ -52,11 +73,13 @@ module.exports = {
 		for (var entityName in tmr) {
 			color[entityName] = generateColor(colorCounter, colorMax);
 			++colorCounter;
+			if (sentOffset && tmr[entityName]["sent-word-ind"][0] == 0)
+				sentOffset = 0;
 		}
 
 		for (var entityName in tmr) {
 			var entityData = tmr[entityName];
-			var isEvent = (entityData["is-in-subtree"] == "EVENT");
+			var isObject = (entityData["is-in-subtree"] == "OBJECT");
 			var required = [];
 			var optional = [];
 			var debug = [];
@@ -70,19 +93,18 @@ module.exports = {
 				// Simply check and extract the contained string if attrVal is not a string
 				if (relations.has(attrKey)) {
 					if ( !(typeof attrVal === 'string') && !(attrVal instanceof String))
-						attrVal = attrVal.value;
-					if (isEvent)
-						insertAfter.push(attrVal);
+						attrVal = attrVal.VALUE;
+					//if (!isObject)
+					//	insertAfter.push(attrVal);
 				}
 
 				var attr = {"_key": attrKey, "_val": insertLinebreaks(attrVal)};
 
-				if (entitySet.has(attrVal))
+				// associate token with entity color
+				if (attrKey == "sent-word-ind")
+					sentences[attrVal[0]+sentOffset].words[attrVal[1]]._color = color[entityName];
+				else if (entitySet.has(attrVal))
 					attr._color = color[attrVal];
-
-				// associate token with entity identifier (name) and color
-				if (attrKey == "word-ind")
-					splitSentence[attrVal]._color = color[entityName];
 
 				// push entries into appropriate array, based on capitalization/debug
 				if (utils.isCapitalized(attrKey))
@@ -100,18 +122,20 @@ module.exports = {
 					"required": required,
 					"optional": optional,
 					"debug": debug
-				},
-				"insertAfter": insertAfter
+				}
+				//},
+				//"insertAfter": insertAfter
 			});
 		}
 
 		// Sort the frames such that events come first
-
+		frames = sortFrames(frames);
+		console.log(frames);
 		// Return the annotated set along with the collection of
 		// known entities, as well as the sentence itself.
 		return {
 			"_sentenceId": sentenceId,
-			"splitSentence": splitSentence,
+			"sentences": sentences,
 			"_tmrIndex": tmrIndex,
 			"frames": frames
 		};
